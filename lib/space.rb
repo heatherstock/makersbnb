57055b5cd205c1518caa21625a4eee0a7ab5bb9c
Noel Vock <noel1uk@gmail.com>
2018-02-27 17:29:51 +0000

    Edited user class to stop it making a many to many table.

--- lib/space.rb
+++ lib/space.rb
@@ -7,12 +7,6 @@ class Space
   property :id,         Serial
   property :address,    String
   property :price,      Float
-  property :image,      String
 
   belongs_to :user # defaults to :required => true
-
-  # def self.popular
-  #   all(:user_id.gt => 3)
-  # end
-
 end
