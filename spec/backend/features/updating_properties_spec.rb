f02caa4c43557ee1678ac97938f74eaf8383cf77
Stephen Tilley <stephentilley@gmail.com>
2018-02-28 16:53:21 +0000

    Fixing property update test

--- spec/backend/features/updating_properties_spec.rb
+++ spec/backend/features/updating_properties_spec.rb
@@ -1,6 +1,6 @@
 feature 'profile properties' do
   scenario 'users can update their properties' do
-    visit ('/')
+    visit '/'
     fill_in 'username', with: 'Jim'
     fill_in 'password', with: 'abc123'
     click_button('SIGN IN')
@@ -9,9 +9,11 @@ feature 'profile properties' do
     fill_in 'image', with: 'www.imgur.com'
     click_button('CREATE SPACE')
     click_button('USER PROFILE')
-    fill_in 'address', with: '72 Sunny Grove'
-    fill_in 'price', with: '7'
-    click_button('Update')
+    within('#2') do
+      fill_in 'address', with: '72 Sunny Grove'
+      fill_in 'price', with: '7'
+      click_button('Update')
+    end
     expect(page).to have_content('72 Sunny Grove')
   end
 end
