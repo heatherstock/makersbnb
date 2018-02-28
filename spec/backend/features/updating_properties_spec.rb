07899b53651f28206c57c38cdbd9162036016958
Stephen Tilley <stephentilley@gmail.com>
2018-02-28 15:21:43 +0000

    working on fixing test

--- spec/backend/features/updating_properties_spec.rb
+++ spec/backend/features/updating_properties_spec.rb
@@ -3,15 +3,15 @@ feature 'profile properties' do
     visit ('/')
     fill_in 'username', with: 'Jim'
     fill_in 'password', with: 'abc123'
-    click_button('Submit')
+    click_button('SIGN IN')
     fill_in 'address', with: '27 Snowy road'
     fill_in 'price', with: '100'
     fill_in 'image', with: 'www.imgur.com'
     click_button('CREATE SPACE')
-    click_button('Profile')
-    fill_in '2-address', with: '72 Sunny Grove'
-    fill_in '2-price', with: '7'
-    click_button('2-Update')
+    click_button('USER PROFILE')
+    fill_in 'address', with: '72 Sunny Grove'
+    fill_in 'price', with: '7'
+    click_button('Update')
     expect(page).to have_content('72 Sunny Grove')
   end
 end
