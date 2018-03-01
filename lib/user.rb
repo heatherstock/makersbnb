6f52f5cde4c09059c21015a49dd47f76b84a04ec
Stephen Tilley <stephentilley@gmail.com>
2018-03-01 15:28:18 +0000

    comented out encryption stuff

--- lib/user.rb
+++ lib/user.rb
@@ -4,7 +4,7 @@ require 'dm-postgres-adapter'
 
 class User
 
-  attr_reader :password
+  # attr_reader :password
 
   include DataMapper::Resource
 
@@ -14,22 +14,17 @@ class User
   property :username,   String, required: true, unique: true
   property :email,      String, format: :email_address, required: true, unique: true
   property :password,   String, :length => 60, required: true
-  property :password_digest, Text
-
-  validates_confirmation_of :password
-
-  def password=(password)
-    @password = password
-    self.password_digest = BCrypt::Password.create(password)
-  end
-
+  # property :password_digest, Text
+  #
+  # validates_confirmation_of :password
+  #
+  # def password=(password)
+  #   @password = password
+  #   self.password_digest = BCrypt::Password.create(password)
+  # end
+  #
   def self.authenticate(username, password)
-    user = first(username: username)
-    if user && BCrypt::Password.new(user.password_digest) == password
-      user
-    else
-      nil
-    end
+    first(username: username)
   end
 
 end
