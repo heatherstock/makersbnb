c049080876823ebaeb15aa36c48e450f8017082a
Noel Vock <noel1uk@gmail.com>
2018-03-01 12:24:20 +0000

    First request booking test passed.

---
+++ spec/backend/features/request_booking_spec.rb
@@ -0,0 +1,9 @@
+feature 'requests can be made before booking' do
+ scenario 'user makes request to space owner' do
+   sign_in_bob
+     within("#1") do
+       click_button('Request')
+     end
+   expect(page).to have_content('Request sent!')
+ end
+end
